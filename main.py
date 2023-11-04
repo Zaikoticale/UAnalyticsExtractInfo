0ac89db1896262032dc68d4c1051dd16abd554ba
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-11-03 19:12:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from src.controller.analytics_controller import AnalyticsController
 from src.infrastructure.google_analytics_client import GoogleAnalyticsClient
 from src.view.console_view import ConsoleView
 
-
 credentials_path = '/Users/r360sas/Sites/AnalyticsBackup/src/controller/credentials.json'
 analytics_client = GoogleAnalyticsClient(credentials_path)
+
+# Crear el caso de uso y pasar el cliente de Google Analytics
 analytics_usecase = AnalyticsUsecase(analytics_client)
+analytics_controller = AnalyticsController(analytics_usecase)
 
 report = AnalyticsReport(
     group='analytics',
     type='report',
-    name='reportexd',
+    name='reporte',
     view_id='216016069',
-    start_date='2022-01-01',
-    end_date='2022-12-31',
+    start_date='2018-01-01',
+    end_date='2023-07-31',
     metrics=[
         {'expression': 'ga:newUsers'},
         {'expression': 'ga:sessions'},
@@ -29,14 +31,26 @@ report = AnalyticsReport(
         {'expression': 'ga:pageviews'},
     ],
     dimensions=[
-        {'name': 'ga:operatingSystem'},
         {'name': 'ga:year'},
+        {'name': 'ga:operatingSystem'},
+        {'name': 'ga:yearMonth'},
+        {'name': 'ga:date'},
+
+
     ]
 )
-backup_folder = '/Users/r360sas/Sites/AnalyticsBackup/backup'
-report_df = analytics_usecase.fetch_report(report, backup_folder)
 
-analytics_usecase.fetch_report(report, backup_folder)
+# Obtener el informe y guardar los datos en una variable
+report_data = analytics_controller.fetch_report(report)
+
+# Definir la carpeta de respaldo
+backup_folder = '/Users/r360sas/Sites/AnalyticsBackup/backup'
+
+# Guardar los datos del informe en un archivo de Excel en la carpeta de respaldo
+analytics_controller.save_report_to_excel(report_data, report, backup_folder)
 
 console_view = ConsoleView()
-console_view.display_succes()
+if analytics_controller:
+    console_view.display_succes()
+else:
+    console_view.display_error()
