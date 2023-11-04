e1e5a125cbcf8f4f9159c217b313027b2fec86eb
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-11-03 19:11:45 -0500

    Update analytics_controller.py

--- src/controller/analytics_controller.py
+++ src/controller/analytics_controller.py
@@ -1,4 +1,6 @@
 # analytics_controller.py 
+import os
+import openpyxl
 
 from src.usecases.analytics_usecase import AnalyticsUsecase
 
@@ -7,21 +9,22 @@ class AnalyticsController:
   def __init__(self, usecase: AnalyticsUsecase):  
     self.usecase = usecase
 
-  def fetch_report(self, report, backup_folder):
+  def fetch_report(self, report):
     data = self.usecase.fetch_report(report)
+    return data
     
-def save_report_to_excel(self, report_df, report, backup_folder):
+  def save_report_to_excel(self, report_df, report, backup_folder):
     workbook = openpyxl.Workbook()
 
     for dimension in report.dimensions[1:]:
-        sheet_name = dimension['name'].replace('/', '_').replace(':', '_').replace('ga', '')
+        sheet_name = dimension['name'].replace('/', '_').replace(':', '_').replace('ga', '').replace('_','')
         workbook.create_sheet(sheet_name)
 
         metrics = report.metrics
         sheet = workbook[sheet_name]
-        sheet.append(['Fecha'] + [metric['expression'] for metric in metrics])
+        sheet.append(['Fecha', 'Dimensión'] + [metric['expression'] for metric in metrics])
 
-        for index, row in report_df.iterrows():
+        for _, row in report_df.iterrows():
             sheet.append([row['dimensions'][0]] + row['metrics'][0]['values'])
 
     if not os.path.exists(backup_folder):
@@ -29,7 +32,4 @@ def save_report_to_excel(self, report_df, report, backup_folder):
 
     excel_file_path = os.path.join(backup_folder, 'Reporte.xlsx')
     workbook.save(excel_file_path)
-    return data
-
-
-
+    return
