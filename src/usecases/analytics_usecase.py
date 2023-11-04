2b7dd19980395d0cf0086f00c22cf680417e2f2d
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-11-03 19:12:26 -0500

    Update analytics_usecase.py

--- src/usecases/analytics_usecase.py
+++ src/usecases/analytics_usecase.py
@@ -1,10 +1,6 @@
-import os
-import openpyxl
-
 class AnalyticsUsecase:
     def __init__(self, analytics_client):
         self.analytics_client = analytics_client
 
-    def fetch_report(self, report, backup_folder):
-        report_df = self.analytics_client.fetch_report(report)
-        
+    def fetch_report(self, report):
+        return self.analytics_client.fetch_report(report)
