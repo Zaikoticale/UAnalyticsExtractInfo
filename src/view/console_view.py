54d0ada78ba24fab636383e87d1fa069a1f09673
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-11-27 15:06:19 -0500

    Update console_view.py

--- src/view/console_view.py
+++ src/view/console_view.py
@@ -1,6 +1,16 @@
+import pyfiglet
+name = pyfiglet.figlet_format("Analytics Backup", font="small")
+by = pyfiglet.figlet_format("by Zaikoticale")
+
 class ConsoleView:
     def display_succes(self):
-        print("Success!", 403)
+        print("\033[1m\033[91m")
+        print(name)
+        print(by)
+        print("\033[92m")
+        print("Success!")
+        print("The report was saved in the backup folder.")
+        print("\033[92m\033[1mDone!\033[0m")
 
-        def display_error(self, error):
-            print(f"Error: {error_code}")
+    def display_error(self, error):
+        print(f"Error: {error}")
