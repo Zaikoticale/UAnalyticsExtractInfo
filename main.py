83e5297e83eb570cf3cd2bb7cde6c46cd043e6d5
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-11-27 15:10:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,54 +3,58 @@ from src.usecases.analytics_usecase import AnalyticsUsecase
 from src.controller.analytics_controller import AnalyticsController
 from src.infrastructure.google_analytics_client import GoogleAnalyticsClient
 from src.view.console_view import ConsoleView
+from datetime import datetime
 
-credentials_path = '/Users/r360sas/Sites/AnalyticsBackup/src/controller/credentials.json'
+credentials_path = 'src/controller/credentials.json'
 analytics_client = GoogleAnalyticsClient(credentials_path)
 
 # Crear el caso de uso y pasar el cliente de Google Analytics
+
 analytics_usecase = AnalyticsUsecase(analytics_client)
 analytics_controller = AnalyticsController(analytics_usecase)
+backup_folder = 'backup/Hora'
 
+group = 'mes'  
+type = 'Usuario'
 report = AnalyticsReport(
-    group='analytics',
-    type='report',
-    name='reporte',
-    view_id='216016069',
+    group=group,
+    type=type,
+    name=f'{type} por {group}',
+    view_id='77379827',
     start_date='2018-01-01',
-    end_date='2023-07-31',
+    end_date='2023-06-30',
+    pageSize=1000000000,
     metrics=[
-        {'expression': 'ga:newUsers'},
-        {'expression': 'ga:sessions'},
+        # <--------- Comportamiento ----------->
+        # {'expression': 'ga:timeOnPage'},
+        # {'expression': 'ga:totalEvents'},
+        # {'expression': 'ga:uniqueEvents'},
+        # {'expression': 'ga:sessionsWithEvent'}
+
+        # <------------ Usuarios ---------------->
         {'expression': 'ga:sessionDuration'},
         {'expression': 'ga:avgSessionDuration'},
         {'expression': 'ga:sessionsPerUser'},
+        {'expression': 'ga:pageviewsPerSession'},
         {'expression': 'ga:bounceRate'},
         {'expression': 'ga:avgTimeOnPage'},
         {'expression': 'ga:sessions'},
         {'expression': 'ga:users'},
-        {'expression': 'ga:pageviews'},
-    ],
+        {'expression': 'ga:newUsers'},
+        {'expression': 'ga:pageviews'}
+        ],
     dimensions=[
-        {'name': 'ga:year'},
-        {'name': 'ga:operatingSystem'},
-        {'name': 'ga:yearMonth'},
-        {'name': 'ga:date'},
-
-
+    #{'name': 'ga:date'}
+    {'name': 'ga:yearMonth'},
     ]
 )
 
-# Obtener el informe y guardar los datos en una variable
+
 report_data = analytics_controller.fetch_report(report)
-
-# Definir la carpeta de respaldo
-backup_folder = '/Users/r360sas/Sites/AnalyticsBackup/backup'
-
-# Guardar los datos del informe en un archivo de Excel en la carpeta de respaldo
-analytics_controller.save_report_to_excel(report_data, report, backup_folder)
+analytics_controller.save_report_to_csv(report_data, report, backup_folder)
 
 console_view = ConsoleView()
 if analytics_controller:
     console_view.display_succes()
 else:
-    console_view.display_error()
+    console_view.display_error('error')
