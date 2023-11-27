33f530bd6a0fe3c874afb08ee20921b4f1ea93b7
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-11-27 15:04:38 -0500

    Update analytics_controller.py

--- src/controller/analytics_controller.py
+++ src/controller/analytics_controller.py
@@ -1,35 +1,71 @@
-# analytics_controller.py 
 import os
 import openpyxl
-
+import pandas as pd
 from src.usecases.analytics_usecase import AnalyticsUsecase
+import csv
 
 class AnalyticsController:
 
-  def __init__(self, usecase: AnalyticsUsecase):  
-    self.usecase = usecase
+    def __init__(self, usecase: AnalyticsUsecase):
+        self.usecase = usecase
 
-  def fetch_report(self, report):
-    data = self.usecase.fetch_report(report)
-    return data
-    
-  def save_report_to_excel(self, report_df, report, backup_folder):
-    workbook = openpyxl.Workbook()
+    def fetch_report(self, report):
+        data = self.usecase.fetch_report(report)
+        return data
 
-    for dimension in report.dimensions[1:]:
-        sheet_name = dimension['name'].replace('/', '_').replace(':', '_').replace('ga', '').replace('_','')
-        workbook.create_sheet(sheet_name)
+    def save_report_to_csv(self, report_df, report, backup_folder: str):
+        workbook = openpyxl.Workbook()
+        default_sheet = workbook.active
+        workbook.remove(default_sheet)
+
+        if len(report.dimensions) > 1:
+            second_dimension_name = report.dimensions[1]['name'].replace('/', '_').replace(':', '_').replace('ga', '').replace('_','')
+        else:
+            second_dimension_name = None
+
+        sheet = workbook.create_sheet(second_dimension_name)
 
         metrics = report.metrics
-        sheet = workbook[sheet_name]
-        sheet.append(['Fecha', 'Dimensión'] + [metric['expression'] for metric in metrics])
 
-        for _, row in report_df.iterrows():
-            sheet.append([row['dimensions'][0]] + row['metrics'][0]['values'])
+        sheet.append(['pagina'] + [metric['expression'].replace('ga:', '') for metric in metrics])
 
-    if not os.path.exists(backup_folder):
-        os.makedirs(backup_folder)
+        for i, row in report_df.iterrows():
+            dimensions = row['dimensions']
+            metrics_values = row['metrics'][0]['values']
+            if second_dimension_name == 'date':
+                if 'Unnamed: 11' in row['dimensions']:
+                    row['dimensions'][row['dimensions'].index(' ')] = 'date'
+                sheet.append([dimensions[0].replace('ga:', '')] + [value.replace('ga:', '') for value in metrics_values] + [dimensions[-1].replace('ga:', '')])
+            else:
+                sheet.append([dimensions[0].replace('ga:', '')] + [value.replace('ga:', '') for value in metrics_values])
 
-    excel_file_path = os.path.join(backup_folder, 'Reporte.xlsx')
-    workbook.save(excel_file_path)
-    return
+        sheet.auto_filter.ref = sheet.dimensions
+
+        group_folder = os.path.join(backup_folder, report.group)
+        type_folder = os.path.join(group_folder, report.type)
+
+
+        if not os.path.exists(group_folder):
+            os.makedirs(group_folder)
+
+        if not os.path.exists(type_folder):
+            os.makedirs(type_folder)
+
+        csv_file_path = os.path.join(type_folder, f"{report.name}.csv")
+
+        with open(csv_file_path, 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['pagina'] + [metric['expression'].replace('ga:', '') for metric in metrics])
+
+            for i, row in report_df.iterrows():
+                dimensions = row['dimensions']
+                metrics_values = row['metrics'][0]['values']
+                if second_dimension_name == 'date':
+                    if 'Unnamed: 11' in row['dimensions']:
+                        row['dimensions'][row['dimensions'].index(' ')] = 'date'
+                    writer.writerow([dimensions[0].replace('ga:', '')] + [value.replace('ga:', '') for value in metrics_values] + [dimensions[-1].replace('ga:', '')])
+                else:
+                    writer.writerow([dimensions[0].replace('ga:', '')] + [value.replace('ga:', '') for value in metrics_values])
+
+        return
+
